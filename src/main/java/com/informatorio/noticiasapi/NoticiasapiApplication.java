d17db2f03ffa1673f5cc09c26b0e4a4b7445a3d9
Julian Palavecino <samuguayfenix@gmail.com>
2022-08-17 19:37:40 -0300

    Added spring base project

---
+++ src/main/java/com/informatorio/noticiasapi/NoticiasapiApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.noticiasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/noticiasapi/NoticiasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.noticiasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
